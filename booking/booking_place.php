5ec82886b447908a230afcbefbf97791da6f26a6
sakhalin <73.vorobyev@gmail.com>
2021-03-15 15:14:00 +0300

    microfix update balance style

---
+++ booking/booking_place.php
@@ -0,0 +1,18 @@
+<?php require __DIR__ . '/.header.php'; ?>
+<div class="other_page_top_wrapper">
+    <div class="other_page_title_wrapper">
+        <a href="#" class="back">
+            <img src="/img/back.svg" alt="arrow-back">
+        </a>
+        <div class="title">
+            Выбор центра
+        </div>
+    </div>
+</div>
+<div class="booking_place_main_content_wrapper">
+    <div class="booking_place_form_wrapper">
+
+    </div>
+</div>
+
+<?php require __DIR__ . '/.footer.php'; ?>
